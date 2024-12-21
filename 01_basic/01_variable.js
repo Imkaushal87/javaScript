d050c2047d2b9bb7f5668b587b58ef92e72bb17d
Kuvar Kaushal Patel <kaushal1094iiit@gmail.com>
2024-12-21 20:38:34 +0000

    container config done

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,19 @@
+const accountId = 12223
+let accountEmail = "kuvar@gmail.com"
+var accountPassword = "kaushal@1234"
+accountCity = "varanasi"
+let accountState;
+
+// accountId =2 not allow because const variable
+accountEmail = "kaushal@gmail.com"
+accountPassword = "122345"
+accountCity = "kota"
+
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+console.log([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+
+
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict";//treat all js code as newer version
+// alert (3+3)// we are using node js not browser
+
+
+
+//number 2 power 53
+//bigInt 
+//string =>""
+// boolen => true/false
+//null => standalone value
+//undefined
+//symbole =>unique
+
+
+
+// object
